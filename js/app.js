14f1c4821eac7764542cb25708ac227be58e01d4
rajmahal <rmahal25@gmail.com>
2018-08-19 15:42:12 -0700

    Working on media query for mobile

--- js/app.js
+++ js/app.js
@@ -44,5 +44,16 @@ $(document).ready(function() {
     });
 
 
+
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+
+
     
 });
