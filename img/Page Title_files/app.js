3fc991edb7ef75cc4069f0c3aea67648a938f76b
rajmahal <rmahal25@gmail.com>
2018-08-20 01:51:20 -0700

    Mobile adjustments finished, working on footer

--- img/Page Title_files/app.js
+++ img/Page Title_files/app.js
@@ -61,20 +61,36 @@ $(document).ready(function() {
 
 
 
-    var modal = document.getElementById('modalOne');
-    var imgOne = document.getElementById('kirby');
-    var modalImg = document.getElementById("kirb1");
-    imgOne.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-    }
+// Get the modal
+var modal = document.getElementById('myModal');
 
-    var span = document.getElementsByClassName("close")[0];
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-    span.onclick = function() { 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
         modal.style.display = "none";
     }
+}
 
 
 
+
+
+
+    
 });
